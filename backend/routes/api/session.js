8a19d8c79b84114a1588a37aba708d4c09701e1c
ChrisEkeDev <iam@chriseke.com>
2023-07-11 13:20:15 -0500

    Finished validators for sign up and sign in

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 
@@ -21,8 +23,18 @@ router.get('/', (req, res) => {
     }
   );
 
+
+// Validate sign in
+const validateSignIn = [
+  check('email').exists({ checkFalsy: true }).notEmpty().isEmail()
+    .withMessage('Please provide a valid email or username.'),
+  check('password').exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+
 // Route for user sign in
-router.post('/', async(req, res, next) => {
+router.post('/', validateSignIn, async(req, res, next) => {
     const { email, password } = req.body;
 
     const user = await User.unscoped().findOne({
